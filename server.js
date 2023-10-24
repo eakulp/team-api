26d04d114a2f9c4797bd39d0419c96d179728e5c
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2023-10-24 15:21:40 -0500

    nathan's GET path

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const app = express();
 app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/brodie', (req,res) => {res.send("Hello Brodie")});
+app.get('/nathan', (req,res) => {res.send("Hello Nathan")});
 
 app.listen(port, ()=>console.log("Listening"));
