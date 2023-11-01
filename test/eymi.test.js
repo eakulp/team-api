d7a4a00cf6b050e4ae07782924dd3e413e4ffc25
eakulp <eymixita@gmail.com>
2023-11-01 14:22:51 -0700

    eymis updated

--- test/eymi.test.js
+++ test/eymi.test.js
@@ -1,7 +1,8 @@
-const response = await fetch('http://localhost:3000/eymi');
-const getName = await response.text();
+const fetch = require("node-fetch2");
 
 //testing testing
-it("gets the correct name", ()=>{
+it("gets the correct name", async ()=>{
+    const response = await fetch('http://localhost:3000/eymi');
+    const getName = await response.text();
     expect(getName).toBe("Hello Eymi");
 })
