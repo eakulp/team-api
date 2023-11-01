1dd572feeeba40dcaeef2143adfdf1c5c4673f8f
brodiembusby <brodiembusby@hotmail.com>
2023-11-01 15:17:59 -0600

    added new fetch

--- test/brodie.test.js
+++ test/brodie.test.js
@@ -1,7 +1,8 @@
-const response = await fetch('http://localhost:3000/brodie');
-const getName = await response.text();
+const fetch = require("node-fetch2");
 
 //Brodie testing
-it("gets the correct name", ()=>{
+it("gets the correct name", async ()=>{  
+    const response = await fetch('http://localhost:3000/brodie');
+    const getName = await response.text();
     expect(getName).toBe("Hello Brodie");
 })
